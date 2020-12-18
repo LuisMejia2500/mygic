4f4aa03e14f96377adad135e0ccdc733737ad83c
Luis Gustavo Mejía Barrios <71474291+LuisMejia2500@users.noreply.github.com>
2020-12-18 00:12:54 +0000

    2.3

--- src/App.vue
+++ src/App.vue
@@ -1,90 +1,166 @@
 <template>
   <div id="app">
-        <div class="Header">  
-    <nav>
-      <h1>MyGIC</h1>
-        <button> <router-link to="/inicio">Inicio</router-link></button>
-        <button><router-link to="/crearProducto">Crear</router-link></button>
-        <button><router-link to="/consultarProducto">Consultar</router-link>  </button>
-    </nav>
-  </div>
-  
-    <router-view/>
+    
+      <div class="header">
+        <nav class="estiloNav">
+          <h1 class="nombreApp">MyGIC</h1>
+          <div class="contenedorBotones">
+            <button v-on:click="procesarInicio" class="boton" > Inicio</button>
+            <button v-on:click="procesarCrearProducto" class="boton">Crear</button>
+            <button v-on:click="selectGet" class="boton">Consultar  </button>
+          </div>
+        </nav>
+      </div>
+      <div class="main">
+        <router-view></router-view>
+      </div>
 
-      <div class="footer">    
-     <p>Desarrollado por G2M3Grupo3</p>    
-  </div>
+      <div class="footer">
+        <p>Desarrollado por G2M3Grupo3</p>
 
+      </div>
     
   </div>
 </template>
 
+<script>
 
-<style scoped>
- body{
-      margin: 0 0 0 0;
-      font-family: /*'Josefin Sans', sans-serif,*/ 'Bungee', cursive;
-      background-image: linear-gradient(to bottom right, white 0%, rgb(183, 240, 50) 100%);
+export default {
+  name: 'App',
+  components: {
+    
+  },
 
- }
-   nav{
-       margin-right: 60px;
-       min-height: 100px;
-       background: #99FF99;
-       justify-content: center;
-        margin: 0%;
-        margin-top: 0%;
-        padding: 0;
-        width: 100%;
-        height: 10px;
-        min-height: 100px;
-        left:0%;
+  data: function(){
+  return{
+      home: true,
+      create: false
+    }
+  },
 
-        background-color: #99FF99 ;
-        color:#0f1011  ;
-
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-       
-   }
-   h1{
-       margin-left: 60px;
-       font-size: 40pt; 
-       font-family: 'Bungee';       
-   }
-   button{
-       font-size: 18pt;
-       color: black;
-       background-color: white;
-       border-radius: 20px;
-       padding: 5px 20px;
-       margin:0px;      
-   }
-   .button:hover{
-       background-color: white;
-   } 
-
-   .footer{      
-        background-color: #99FF99;
-        width: 100%;
-        height: 81px;
-        position: absolute;
-        bottom: 0;
-        left: 0; 
-        text-align: center;
-        align-items: center;
-
+  methods:{
+    procesarInicio:function(){
+      //this.$router.push({name: 'Home'})
+      if(this.$route.name != 'home'){
         
+        this.$router.push({name: 'home'})        
+      }
+
+    },
+    
+    procesarCrearProducto:function(){ 
+      
+      if(this.$route.name != 'createProduct'){
+        
+        this.$router.push({name: 'createProduct'})        
+      }         
+      
+    },
+
+    selectGet:function(){
+      if(this.$route.name != 'getProduct'){
+        
+        this.$router.push({name: 'getProduct'})        
+      }  
     }
-    p{
-        font-size: 15pt;
-        color: black;
-        margin-top: 2.5%;    
-    }
-    nav{
-      text-decoration: none;
-      text-decoration-style: none;
-    }
+  },
+
+  created: function(){
+    
+    this.$router.push({name: 'home'})        
+    
+  }
+}
+
+</script>
+
+<style>
+#app{
+  background-image: linear-gradient(to bottom right, white 0%, rgb(183, 240, 50) 100%);
+}
+
+.estiloBody{
+  margin: 0 0 0 0; 
+  background-color: -webkit-linear-gradient(top, #f00 0%, #00f 100%);;
+}
+.header{
+  width: 90%;
+  border-radius: 5px;
+  margin: auto;
+  border: 1px solid #000000;
+  display: flex;
+}
+.estiloNav{
+  background-color:  #99FF99;
+  width: 100%;
+  padding: 5px;
+  margin: 5px;
+  display: flex; 
+  align-items: center; 
+}
+.nombreApp{
+  margin-left: 60px;
+  font-size: 40pt; 
+  font-family: 'Langar', cursive;
+}
+.contenedorBotones{
+  margin-left: 50%;
+}
+.boton{
+  font-family: Arial;
+    color: #ffffff;
+    font-size: 15px;
+    padding: 10px;
+    text-decoration: none;
+    -webkit-border-radius: 28px;
+    -moz-border-radius: 28px;
+    border-radius: 28px;
+    -webkit-box-shadow: 0px 1px 3px #666666;
+    -moz-box-shadow: 0px 1px 3px #666666;
+    box-shadow: 0px 1px 3px #666666;
+    text-shadow: 1px 1px 3px #666666;
+    border: solid #2a5c28 2px;
+    background: #2a5c28;
+  /*border-radius: 20px;
+  padding: 5px 20px;
+  margin:0px; */
+  
+}
+.boton:hover{
+  background-color: #00A86B;
+}
+.main{
+  width: 90%;
+  background-color: #ffff80;
+  margin: auto;
+  margin-top: 20px;
+  border-radius: 5px;
+  border: 1px solid #000000;
+
+  display: flex;
+  justify-content: center;
+}
+.footer{      
+  background-color: #99FF99;
+  width: 90%;
+  
+    
+  margin: auto;
+  border-radius: 5px;
+  border: 1px solid #000000;
+  margin-top: 10px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  /*margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 10vh;
+    min-height: 100px;
+    background-color: #283747;
+    color: #E5E7E9;*/
+}
 
 </style>
